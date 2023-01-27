afaaec64c5c800cf3ce95e21fccfdf30b8876294
Everton Barros <evbarros@deloitte.com>
2023-01-27 15:35:20 -0300

    ajustando rest quarkus.

---
+++ src/main/java/org/acme/entity/Fruit.java
@@ -0,0 +1,14 @@
+package org.acme.entity;
+
+public class Fruit {
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
